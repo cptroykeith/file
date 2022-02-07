6c3ade62873f3fb6eb456c5362ea9ab711c820a4
your-name <your-email-address>
2022-02-07 13:28:43 +0300

    reading the *whole* file

--- file.py
+++ file.py
@@ -1,6 +1,14 @@
 #counting line in a file
+'''
 fhand = open('mbox.txt')
 count = 0
 for line in fhand:
     count = count + 1
 print('line count:', count)
+'''
+#reading the *whole* file
+fhand = open ('mbox-short.txt')
+inp = fhand.read()
+print(len(inp))
+print(inp[:20])
+
